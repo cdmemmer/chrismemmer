b672e3c2ad2f28a7aa2f9d06c6840b68c55fb11e
cdmemmer <122756135+cdmemmer@users.noreply.github.com>
2023-04-01 19:42:52 -0500

    Add files via upload

---
+++ JavaScript/dailyinfo.js
@@ -0,0 +1,125 @@
+if (month == "March" && day1=="31") {
+    document.getElementById("wordOfDay").innerHTML = "Abate - become less intense or widespread";
+    document.getElementById("factOfDay").innerHTML = "George Washington was the only president unanimously elected. Meaning all of the state representatives voted for him.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "1") {
+    document.getElementById("wordOfDay").innerHTML = "Berate – scold or criticize";
+    document.getElementById("factOfDay").innerHTML = "John Adams died on the same day as Thomas Jefferson, July 4th, 1826. This day was also the 50th anniversary of the approval of the Declaration of Independence";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "2") {
+    document.getElementById("wordOfDay").innerHTML = "Cache – a collection of similar items stored in a secret place";
+    document.getElementById("factOfDay").innerHTML = "James Madison and George Washington are the only presidents who signed the Constitution";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "3") {
+    document.getElementById("wordOfDay").innerHTML = "Bias - cause to feel or show inclination or prejudice for or against someone or something";
+    document.getElementById("factOfDay").innerHTML = "James Madison was the shortest president at 5 feet 4 inches tall and weighed 100 pounds. Abraham Lincoln was the tallest president at 6 feet 4 inches tall. Lyndon B. Johnson was also 6' 4 ";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "4") {
+    document.getElementById("wordOfDay").innerHTML = "Palatial – resembling a palace";
+    document.getElementById("factOfDay").innerHTML = "James Monroe was the 5th president, but the 3rd to die on the 4th of July";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "5") {
+    document.getElementById("wordOfDay").innerHTML = "The - used as a function word to indicate that a following noun or noun equivalent is definite or has been previously specified by context or by circumstance";
+    document.getElementById("factOfDay").innerHTML = "Franklin D. Roosevelt was the first president to appear on television during a 1939 broadcast from the World's Fair";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "6") {
+    document.getElementById("wordOfDay").innerHTML = "Chair – a separate seat for one person, typically with a back and four legs.";
+    document.getElementById("factOfDay").innerHTML = "At 42 years, 10 months, 18 days old Teddy Roosevelt was the youngest man to hold the office of president. Joe Biden was the oldest at 78 years, 61 days. John F. Kennedy was the youngest to be elected president";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "7") {
+    document.getElementById("wordOfDay").innerHTML = "Lottle – a lot of something (I made this up)";
+    document.getElementById("factOfDay").innerHTML = "John F. Kennedy was the first president who was a Boy Scout";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "8") {
+    document.getElementById("wordOfDay").innerHTML = "Laptop – a small, portable personal computer.";
+    document.getElementById("factOfDay").innerHTML = "George W. Bush is the only president to have earned a Master of Business Administration (MBA) degree";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "9") {
+    document.getElementById("wordOfDay").innerHTML = "Book –a written or printed work consisting of pages glued or sewn together along one side and bound in covers";
+    document.getElementById("factOfDay").innerHTML = "Martin Van Buren was the only president to speak English as a second language. His first language was Dutch";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "10") {
+    document.getElementById("wordOfDay").innerHTML = "Kismet – destiny";
+    document.getElementById("factOfDay").innerHTML = "William Henry Harrison was the 9th president. His grandson, Benjamin Harrison, was the 23rd president";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "11") {
+    document.getElementById("wordOfDay").innerHTML = "Jocular – humorous or playful";
+    document.getElementById("factOfDay").innerHTML = "James K. Polk was the first president to have his photograph taken while in office";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "12") {
+    document.getElementById("wordOfDay").innerHTML = "Lexicon – vocabulary of a person";
+    document.getElementById("factOfDay").innerHTML = "William Henry Harrison died just 32 days after becoming president. He died from a cold he got while standing in the rain giving his inauguration speech";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "13") {
+    document.getElementById("wordOfDay").innerHTML = "Limber – flexible";
+    document.getElementById("factOfDay").innerHTML = "Rutherford B. Hayes was the first US president to use the telephone. ";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "14") {
+    document.getElementById("wordOfDay").innerHTML = "Malapropism – mistaken use of a word in place of a similar sounding word";
+    document.getElementById("factOfDay").innerHTML = "James Buchanan was the only president who never got married, which meant that his niece was the First Lady. ";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "15") {
+    document.getElementById("wordOfDay").innerHTML = "Misnomer – wrong or inaccurate name in designation";
+    document.getElementById("factOfDay").innerHTML = "It was during Bill Clinton’s administration that a website for the White House was created.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "16") {
+    document.getElementById("wordOfDay").innerHTML = "Novel – fictitious prose narrative";
+    document.getElementById("factOfDay").innerHTML = "President Warren G. Harding was the 29th in history and the very first to make an address on the radio in 1922.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "17") {
+    document.getElementById("wordOfDay").innerHTML = "Movie – a story or event recorded by a camera as a set of moving images and shown in a theater or on television; a motion picture";
+    document.getElementById("factOfDay").innerHTML = "President Bill Clinton was the first commander-in-chief to send an email while he was in office in 1994.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "18") {
+    document.getElementById("wordOfDay").innerHTML = "Paramount – more important than anything else";
+    document.getElementById("factOfDay").innerHTML = "There have been two times in history that a father and his son have both been elected president. The two father/son pairs were John Adams and son John Quincy Adams and George H.W. Bush and son George W. Bush.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "19") {
+    document.getElementById("wordOfDay").innerHTML = "Prestidigitation – magic tricks performed for entertainment";
+    document.getElementById("factOfDay").innerHTML = "Congress bought President William Taft a car in 1908, making him the first president to have one while in office.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "20") {
+    document.getElementById("wordOfDay").innerHTML = "Search - try to find something by looking or otherwise seeking carefully and thoroughly";
+    document.getElementById("factOfDay").innerHTML = "Ulysses S. Grant, the 18th president, was given a $20 speeding ticket for riding his horse and buggy too quickly in Washington D.C.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "21") {
+    document.getElementById("wordOfDay").innerHTML = "Resilient – able to withstand";
+    document.getElementById("factOfDay").innerHTML = "Martin Van Buren was the eighth president, but he was the first president to be born as a US citizen.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "22") {
+    document.getElementById("wordOfDay").innerHTML = "Scrupulous – diligent attention to details";
+    document.getElementById("factOfDay").innerHTML = "Abraham Lincoln, was the first president to be born outside of the 13 original colonies. ";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "23") {
+    document.getElementById("wordOfDay").innerHTML = "Terse – sparing in use of words";
+    document.getElementById("factOfDay").innerHTML = "Andrew Johnson was the 17th president and was the first president to be impeached.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "24") {
+    document.getElementById("wordOfDay").innerHTML = "Watch – a small timepiece worn typically on a strap on one's wrist";
+    document.getElementById("factOfDay").innerHTML = "President James K. Polk had the shortest retirement of any president, dying three months after leaving office at age 53.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "25") {
+    document.getElementById("wordOfDay").innerHTML = "Laden – heavily loaded or weighed down";
+    document.getElementById("factOfDay").innerHTML = "John Adams was the second president, but he was the first to live in the White House.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "26") {
+    document.getElementById("wordOfDay").innerHTML = "Name – a word or a combination of words by which a person, place, or thing, a body or class, or any object of thought is designated, called, or known";
+    document.getElementById("factOfDay").innerHTML = "The first president to take in a Major League Baseball game while in office was Benjamin Harrison in 1892.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "27") {
+    document.getElementById("wordOfDay").innerHTML = "Zenith – peak";
+    document.getElementById("factOfDay").innerHTML = "There are 132 rooms, 35 bathrooms, and 6 levels in the White House to accommodate all the residents, workers and guests who live in it.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "28") {
+    document.getElementById("wordOfDay").innerHTML = "Zephyr – soft gentle breeze";
+    document.getElementById("factOfDay").innerHTML = "There are 412 doors, 147 windows, 28 fireplaces, 8 staircases, and 3 elevators in the White House.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "29") {
+    document.getElementById("wordOfDay").innerHTML = "Tenacious – keep a firm hold on something";
+    document.getElementById("factOfDay").innerHTML = "There are 412 doors, 147 windows, 28 fireplaces, 8 staircases, and 3 elevators in the White House.";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "April" && day1 == "30") {
+    document.getElementById("wordOfDay").innerHTML = "Rife – of common occurrence";
+    document.getElementById("factOfDay").innerHTML = "Woodrow Wilson was the first president to travel to Europe while in office";
+    document.getElementById("dayInHistory").innerHTML = "";
+} 
