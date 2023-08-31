7b2a557e86680c81886de87138fe8d369cbaa48b
cdmemmer <122756135+cdmemmer@users.noreply.github.com>
2023-08-31 18:42:52 -0500

    Add files via upload

--- Home/dailyinfo.js
+++ Home/dailyinfo.js
@@ -3,7 +3,7 @@ const d = new Date();
 //Day of week
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = weekday[d.getDay()];
-document.getElementById("dayOfWeek").innerHTML = day;
+document.getElementById("dayOfWeek").innerHTML = day + ",";
 
 //Month
 const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -18,8 +18,124 @@ document.getElementById("day").innerHTML = day1 + ",";
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
 
-if (month == "August" && day1 == "11") {
-    document.getElementById("wordOfDay").innerHTML = "Debouch – emerge from a narrow or confined space into a wide, open area.";
-    document.getElementById("factOfDay").innerHTML = "In the 2019 regular season, the average baseball game lasted for 3 hours, 5 minutes, 35 seconds.";
-    document.getElementById("dayInHistory").innerHTML = "1929 - New York Yankees hitter Babe Ruth becomes the first MLB player to hit 500 home runs in a 6-5 loss to Cleveland";
+if (month == "September" && day1 == "1") {
+    document.getElementById("wordOfDay").innerHTML = "Adjourn - break off (a meeting, legal case, or game) with the intention of resuming it later";
+    document.getElementById("factOfDay").innerHTML = "Chess is a required school subject in Armenia.";
+    document.getElementById("dayInHistory").innerHTML = "1972 - Bobby Fischer became the first American to win the World Chess Championship";
+} else if (month == "September" && day1 == "2") {
+    document.getElementById("wordOfDay").innerHTML = "Bank - a financial establishment that invests money deposited by customers, pays it out when required, makes loans at interest, and exchanges currency";
+    document.getElementById("factOfDay").innerHTML = "Coins can last around 30 years in circulation";
+    document.getElementById("dayInHistory").innerHTML = "1789 - US Treasury Department established by Congress";
+} else if (month == "September" && day1 == "3") {
+    document.getElementById("wordOfDay").innerHTML = "Revolution - a forcible overthrow of a government or social order, in favor of a new system";
+    document.getElementById("factOfDay").innerHTML = "The American Revolutionary war lasted just over seven years";
+    document.getElementById("dayInHistory").innerHTML = "1783 - Treaty of Paris signed in Paris ends the American Revolutionary War between Great Britain and United States of America";
+} else if (month == "September" && day1 == "4") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "5") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "6") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "7") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "8") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "9") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "10") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "11") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "12") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "13") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "14") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "15") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "16") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "17") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "18") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "19") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "20") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "21") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "22") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "23") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "24") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "25") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "26") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "27") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "28") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "29") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
+} else if (month == "September" && day1 == "30") {
+    document.getElementById("wordOfDay").innerHTML = "";
+    document.getElementById("factOfDay").innerHTML = "";
+    document.getElementById("dayInHistory").innerHTML = "";
 }
